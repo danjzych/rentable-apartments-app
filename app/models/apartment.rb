27ac8a732b05ff70348d312440e1eb27adfe5af0
Daniel Zych <danielzych@Daniels-MacBook-Pro.local>
2025-02-12 10:36:09 -0600

    Added basic methods for apartment model

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,2 +1,17 @@
 class Apartment < ApplicationRecord
+  def price_per_sqft
+    rent / sqft
+  end
+
+  def date_available
+    date_available.strftime("%B %d, %Y")
+  end
+
+  def days_on_market
+    (Date.today - date_available).to_i
+  end
+
+  def vacant?
+    date_available < Date.today
+  end
 end
